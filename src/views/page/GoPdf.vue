36023fe941a65b94341ec6a3dd8fe4266971f10d
nhiennnkaopiz <nhiennn@kaopiz.com>
2024-01-03 16:50:38 +0700

    Build v2.0.11

--- src/views/page/GoPdf.vue
+++ src/views/page/GoPdf.vue
@@ -1,38 +1,67 @@
 <template>
-  <div class="content g2-content">
-    <div id="pdf-viewer"></div>
-  </div>
+    <div class="content g2-content">
+
+        <div>
+            <div id="content-viewer">
+            </div>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+#content-viewer {
+    height: 80vh;
+    overflow-y: scroll;
+}
+</style>
 
 <script>
 import { decode64 } from "@utils/AcrouUtil";
-import pdfobject from 'pdfobject'
+import * as pdfjsLib from 'pdfjs-dist/es5/build/pdf';
+import "pdfjs-dist/web/pdf_viewer.css";
+
+pdfjsLib.GlobalWorkerOptions.workerSrc =
+    `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.js`;
+
+
 export default {
-  data: function() {
-    return {};
-  },
-  computed: {
-    url() {
-      if (this.$route.params.path) {
-        return decode64(this.$route.params.path);
-      }
-      return ''
+    data: function () {
+        return {};
+    },
+    computed: {
+        url() {
+            if (this.$route.params.path) {
+                return decode64(this.$route.params.path);
+            }
+            return ''
+        }
+    },
+    mounted() {
+        this.getPdf();
+    },
+    methods: {
+        async getPdf(page) {
+            const loadingTask = pdfjsLib.getDocument(this.url);
+            this.pdfDocument = await loadingTask.promise;
+            this.numPage = this.pdfDocument.numPages
+
+            let viewer = document.getElementById('content-viewer')
+            for (page = 1; page <= this.numPage; page++) {
+                let canvas = document.createElement("canvas");
+                canvas.className = 'pdf-page-canvas';
+                canvas.style.borderBottom = '1px solid #cdcdcd'
+                viewer.appendChild(canvas);
+                this.renderPage(page, canvas, this.scale);
+            }
+        },
+        renderPage(pageNumber, canvas, scale) {
+            this.pdfDocument.getPage(pageNumber).then(function (page){
+                let viewport = page.getViewport({scale: scale});
+                canvas.height = viewport.height;
+                canvas.width = viewport.width;
+                page.render({canvasContext: canvas.getContext('2d'), viewport: viewport});
+            });
+        }
     }
-  },
-  mounted() {
-    pdfobject.embed(this.url, '#pdf-viewer', {height: "100vh"})
-  },
-  methods: {
-    error: function(err) {
-      console.log(err);
-    }
-  }
 };
 </script>
-
-<style scoped>
-object{
-    width: 100%;
-    height: -webkit-fill-available;
-}
-</style>
