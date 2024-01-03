d4c30a26568b376ed3da74daa75390a93f2002b8
nhiennnkaopiz <nhiennn@kaopiz.com>
2024-01-03 16:50:38 +0700

    Build v2.0.11

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app">
-    <Layout ref="layout" />
-  </div>
+    <div id="app">
+        <Layout ref="layout"/>
+    </div>
 </template>
 
 <script>
@@ -9,42 +9,42 @@ import util from "@/libs/util";
 import Layout from "./views/Layout";
 
 export default {
-  name: "App",
-  components: {
-    Layout,
-  },
-  data: function() {
-    return {
-      github: "https://github.com/Aicirou/goindex-theme-acrou",
-    };
-  },
-  watch: {
-    "$i18n.locale": "i18nHandle",
-  },
-  created() {
-    this.i18nHandle(this.$i18n.locale);
-  },
-  mounted() {
-    this.checkVersion();
-  },
-  methods: {
-    i18nHandle(val) {
-      util.cookies.set("lang", val);
-      document.querySelector("html").setAttribute("lang", val);
+    name: "App",
+    components: {
+        Layout,
     },
-    checkVersion() {
-      let g2index_version = window.gdconfig.version;
-      let app_version = process.env.VUE_APP_G2INDEX_VERSION;
-      if (!g2index_version || app_version !== g2index_version) {
-        this.$notify({
-          title: this.$t("notify.title"),
-          dangerouslyUseHTMLString: true,
-          message: this.$t("checkVersion.tips").replace("${url}", this.github),
-          duration: 0,
-          type: "success",
-        });
-      }
+    data: function () {
+        return {
+            github: "https://github.com/Aicirou/goindex-theme-acrou",
+        };
     },
-  },
+    watch: {
+        "$i18n.locale": "i18nHandle",
+    },
+    created() {
+        this.i18nHandle(this.$i18n.locale);
+    },
+    mounted() {
+        this.checkVersion();
+    },
+    methods: {
+        i18nHandle(val) {
+            util.cookies.set("lang", val);
+            document.querySelector("html").setAttribute("lang", val);
+        },
+        checkVersion() {
+            let g2index_version = window.gdconfig.version;
+            let app_version = process.env.VUE_APP_G2INDEX_VERSION;
+            if (!g2index_version || app_version !== g2index_version) {
+                this.$notify({
+                    title: this.$t("notify.title"),
+                    dangerouslyUseHTMLString: true,
+                    message: this.$t("checkVersion.tips").replace("${url}", this.github),
+                    duration: 0,
+                    type: "success",
+                });
+            }
+        },
+    },
 };
 </script>
